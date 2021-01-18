2b3b72a625113158650449aa5cad2a5fbb257472
Shubham92166 <42581060+Shubham92166@users.noreply.github.com>
2021-01-18 14:02:26 +0530

    Filter and search added

---
+++ test/elements/travelSearch.elements.js
@@ -0,0 +1,15 @@
+class travelSearchElements{
+    get searchButton(){
+        return $('//*[@id="root"]/div/div[2]/div/div/div[2]/p/a');
+    }
+    get sortingResult(){
+        return $('//div[@class="sortby-dom-sctn price_sorter  active"]/span[@class="pointer"]');
+    }
+    get allFlightList(){
+        return $$('//div[@class="fli-list-body-section"]');
+    }
+    price(i){
+        return $('(//span[@class="actual-price"])['+i+']');
+    }
+}
+module.exports=new travelSearchElements();
--- wdio.conf.js
+++ wdio.conf.js
@@ -90,7 +90,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost',
+    baseUrl: 'https://www.makemytrip.com',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
@@ -201,6 +201,14 @@ exports.config = {
      */
     // beforeTest: function (test, context) {
     // },
+    beforeTest: function () {
+        const chai=require('chai')
+        const chaiWebdriver=require(chai-Webdriverio).default
+        chai.use(chaiWebdriver(browser))
+        global.assert =chai.assert
+        global.should=chai.should
+        global.expect=chai.expect
+     },
     /**
      * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
      * beforeEach in Mocha)
