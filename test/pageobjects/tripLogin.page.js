8b45827482f59074c80d543ce1f5f27652ba34f3
Sahana-mc <sannumc@gmail.com>
2021-01-18 13:36:23 +0530

    Adding destination and date

--- test/pageobjects/tripLogin.page.js
+++ test/pageobjects/tripLogin.page.js
@@ -1,4 +1,16 @@
+const tripLoginElements = require('../elements/tripLogin.elements')
+
+
 class tripLoginPage{
+    flightBooking()
+    {
+        tripLoginElements.fromcity.click()
+        tripLoginElements.fromLocation.click()
+        tripLoginElements.tocity.click()
+        tripLoginElements.toLocation.click()
+        tripLoginElements.setdate.click()
+        tripLoginElements.searchButton.click()
+    }
 
 }
 module.exports=new tripLoginPage(); 
--- test/specs/tripSpecs.e2e.js
+++ test/specs/tripSpecs.e2e.js
@@ -1,4 +1,13 @@
+const tripLoginElements = require("../elements/tripLogin.elements");
+const tripLoginPage = require("../pageobjects/tripLogin.page");
+const browserUtil = require('../../utilities/browser.util')
+
 describe("Make my trip test cases",function(){
 
+it('Book a flight',function(){
+    browser.url("https://makemytrip.com");
+    tripLoginPage.flightBooking()
+    browser.pause(2000)
+})
 
-});
+})
