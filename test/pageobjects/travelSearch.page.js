04c7059ec0f6221332312393c6e0456a112fc2f1
Shubham92166 <42581060+Shubham92166@users.noreply.github.com>
2021-01-20 12:12:41 +0530

    Added searching flight and sorting the result

--- test/pageobjects/travelSearch.page.js
+++ test/pageobjects/travelSearch.page.js
@@ -1,5 +1,6 @@
 const searchElements=require('../elements/travelSearch.elements');
 const {expect}=require("chai");
+//@Author:Shubham 
 class travelSearchPage{
     searchFlight(){
         searchElements.searchButton.click();
@@ -10,30 +11,12 @@ class travelSearchPage{
     allFlight(){
         let flightList=searchElements.allFlightList;
         for(var i=0;i<flightList.length;i++){
-            //let flightText=flightList[i].getText();
-            //console.log(flightText);
-            /*
-            
-            //expect(priceOfFirstFlight).to.equal(priceOfSecondFlight);
-            let price1=(priceOfFirstFlightText.split('')).split(',');
-            let priceOfFirstFlight=price1[2];
-            console.log(parseInt(priceOfFirstFlight));
-            //expect(priceOfFirstFlight == priceOfSecondFlight, "").to.be.false;
-            console.log(priceOfFirstFlightText);
-            console.log(priceOfSecondFlightText);
-            */
-           var priceOfFirstFlightText=searchElements.price(1).getText();
+            var priceOfFirstFlightText=searchElements.price(1).getText();
            var secondFlight=searchElements.price(5).getText();
-           //console.log("The second flight"+secondFlight);
         let Price= [priceOfFirstFlightText,secondFlight];
-      
-        //console.log("The price array is "+Price)
             let prices = Price.map(element => element.replace(/,/g, "").match(/[0-9]+/));
         
         for (let i=0; i<prices.length-1; i++) {
-        //console.log("Inside loop"+prices[i]);
-
-        //console.log(prices[i+1]);
             expect(prices[i]> prices[i+1]).to.be.true;
 
         }
--- test/specs/travelSpecs.e2e.js
+++ test/specs/travelSpecs.e2e.js
@@ -2,8 +2,9 @@ const searchPage=require('../pageobjects/travelSearch.page');
 const filterPage=require('../pageobjects/travelFilter.page');
 const { allFlight } = require('../pageobjects/travelSearch.page');
 const {expect}=require("chai");
+//@Author:Shubham 
 describe("Flight Booking", function(){
-it("Searching flight", function(){
+    it("Searching flight", function(){
     browser.url('/');
     elem=$('//div[@data-cy="outsideModal"]');
     if(elem.isDisplayed()){
@@ -14,23 +15,9 @@ it("Searching flight", function(){
     searchPage.searchFlight();
     searchPage.sortingFlights();
     allFlight();
-    /*
-    let listOfElements = $$("")
-        let prices = listOfElements.map(element => element.getText().replace(/,/g, "").match(/[0-9]+/)[0])
-        
-        for (let i=0; i<prices.length-1; i++) expect(prices[i]< prices[i+1]).to.be.true
-    let elem =    $("//p[contains(text(), 'Go Air')]/ancestor::label[contains(@for, 'newGroupFilter')]/span/span[@class='check']")
-        elem.click()
-       
-
-        let listOfElements = $$(".airways-name")
-        let listOfFlightName = listOfElements.forEach(elem => {
-            return elem.getText();
-        })
-        for(let flight of listOfFlightName)
-            expect(flight.includes('Go Air'), "Filter is not working.").to.be.true
-        */
     filterPage.filterFlight();
+    filterPage.filterDisplayed();
+    filterPage.filterText();
 
  browser.pause(2000);
 });
