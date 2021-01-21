0ba47ea0c45496d6d0ae1e8e9e76b12a0a9514db
pavan <chinnureddy222@gmail.com>
2021-01-21 14:59:36 +0530

    added assertUtil file and other utilities

--- utilities/browser.util.js
+++ utilities/browser.util.js
@@ -1,14 +1,27 @@
-switchToWindowContaining = (title) => {
-    let AllWindowHandles = browser.getWindowHandles()
-    let currentWindowHandle = browser.getWindowHandle();
-    for (let handle of AllWindowHandles){
-        browser.switchToWindow(handle)
-        if(browser.getTitle().includes(title)) return;
+class BrowserUtil{
+
+    switchToWindowContaining = (title) => {
+        let AllWindowHandles = browser.getWindowHandles()
+        let currentWindowHandle = browser.getWindowHandle();
+        for (let handle of AllWindowHandles){
+            browser.switchToWindow(handle)
+            if(browser.getTitle().includes(title)) return;
+        }
+        browser.switchToWindow(currentWindowHandle);
+        return ;
     }
-    browser.switchToWindow(currentWindowHandle);
-    return ;
+
+    maximizingWindow(){
+        browser.maximizeWindow()
+    }
+
+    getTitle(){
+        return browser.getTitle()
+    }
+
+    reloadPage(){
+        browser.refresh()
+    }
 }
 
-module.exports = {
-    switchToWindowContaining
-}
+module.exports = new BrowserUtil()
