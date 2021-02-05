8ae0aaaefcd329211e9a923f1300f6feca742b38
Rahul Rawat <rawat1416@gmail.com>
2021-02-05 12:43:39 +0530

    database util

--- Utilities/database.js
+++ Utilities/database.js
@@ -1,47 +1,42 @@
-const mongoose = require('mongoose');
-const uri="mongodb+srv://kavya_user01:root@cluster0.u84oj.mongodb.net/cluster0?authSource=admin&replicaSet=atlas-5256ad-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true";
-mongoose.connect(uri, { useNewUrlParser: true }).then(()=>{
-    console.log("Connected");
-}).catch(error => handleError(error));
+const mongoURI =
+  'mongodb+srv://kavya_user01:root@cluster0.u84oj.mongodb.net/cluster0?authSource=admin&replicaSet=atlas-5256ad-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true';
 
-var Schema = mongoose.Schema;
-var JourneySchema = new Schema({
+const insertFlightDetails = (flightDetails) => {
+  const mongoose = require('mongoose');
+  mongoose
+    .connect(mongoURI, { useNewUrlParser: true })
+    .then(() => {
+      console.log('Connected');
+    })
+    .catch((error) => handleError(error));
+
+  const Schema = mongoose.Schema;
+  const JourneySchema = new Schema({
     From: String,
-    To:String,
+    To: String,
     date: {
-        type:Date,
-        default:Date.now
-
-    }
+      type: Date,
+      default: Date.now,
+    },
   });
-  const journey = mongoose.model('SomeModel', JourneySchema );
-  const createDocument=async() =>{
-      try{
-        const journeyDetails= new journey({
-            From:"Dehli",
-            To:"bangalore"
-           
-        })
-        const journeyDetailsOne= new journey({
-            From:"Bangalore",
-            To:"Dehli"
-           
-        })
-        const result=await journey.insertMany([journeyDetails,journeyDetailsOne ]);
-        console.log(result);
-
-      }catch(err){
-          console.log(err);
-      }
-
-      
+  const journey = mongoose.model('SomeModel', JourneySchema);
+  const journeyDetails = new journey(flightDetails);
+  try {
+    journey
+      .insertMany([journeyDetails])
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  } catch (err) {
+    console.log(err);
   }
- 
- createDocument();
+  mongoose.disconnect();
+};
 
+module.exports = {
+  insertFlightDetails,
+};
 
-  
-
-
-
-
+// insertFlightDetails({
+//   From: 'Bangalore',
+//   To: 'Dehli',
+// });
