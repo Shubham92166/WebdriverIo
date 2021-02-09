2ad5856106617171dfdbacdee29aa1812dd0f41e
Shubham92166 <42581060+Shubham92166@users.noreply.github.com>
2021-02-09 09:20:26 +0530

    changes

--- utilities/browser.util.js
+++ utilities/browser.util.js
@@ -1,26 +1,14 @@
-class BrowserUtil {
-	switchToWindowContaining = (title) => {
-		let AllWindowHandles = browser.getWindowHandles();
-		let currentWindowHandle = browser.getWindowHandle();
-		for (let handle of AllWindowHandles) {
-			browser.switchToWindow(handle);
-			if (browser.getTitle().includes(title)) return;
-		}
-		browser.switchToWindow(currentWindowHandle);
-		return;
-	};
-
-	maximizingWindow() {
-		browser.maximizeWindow();
-	}
-
-	getTitle() {
-		return browser.getTitle();
-	}
-
-	reloadPage() {
-		browser.refresh();
-	}
+switchToWindowContaining = (title) => {
+    let AllWindwoHandles = browser.getWindowHandles()
+    let currentWindowHandle = browser.getWindowHandle();
+    for (let handle of handles){
+        browser.switchToWindow(handle)
+        if(browser.getTitle().includes(title)) return;
+    }
+    browser.switchToWindow(currentWindowHandle);
+    return ;
 }
 
-module.exports = new BrowserUtil();
+module.exports = {
+    switchToWindowContaining
+}
