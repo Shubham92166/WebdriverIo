388d2cdefc6073c7cd9a39a6bebcb21c27da6466
AT877 <80334614+AT877@users.noreply.github.com>
2021-04-01 13:03:19 +0530

    Create paytmMovie.js

---
+++ test/specs/paytmMovie.js
@@ -0,0 +1,21 @@
+//Anmol
+const ptm = require("../pageobjects/paytm.page");
+describe("paytm movies",function() {
+    
+    it("open",function() {
+        browser.url("https://www.paytm.com");
+
+        browser.pause(2000);
+
+        ptm.sel_movies();
+
+        browser.pause(2000);
+        ptm.selectCity();
+        
+        ptm.bookMovieTickets();
+
+        ptm.sel_time();
+        
+    })
+
+});
